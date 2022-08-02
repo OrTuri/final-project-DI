fc2703b263ec7afd5dbed0e8c8552058c4a17948
ortori <ortori@gmail.com>
2022-08-02 12:55:38 +0300

    added a form to the nutrition page

--- client/src/components/nutrition/NutritionContainer.js
+++ client/src/components/nutrition/NutritionContainer.js
@@ -1,10 +1,47 @@
 import style from "./NutritionContainer.module.css";
 import Container from "../UI/container/Container";
+import Button from "../UI/Form/Button";
+import Input from "../UI/Form/Input";
+import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
 
 const NutritionContainer = () => {
   return (
     <Container>
-      <h1>NUTRITION</h1>
+      <h1 className={style["main-heading"]}>Nutrition</h1>
+      <Link to="/home" className={style.link}>
+        <Button label="Go Back" width="100px" />
+      </Link>
+      <form className={style.form}>
+        <div>
+          <label htmlFor="foodInput">Food</label>
+          <Input
+            required
+            margin="0"
+            placeholder="Banana"
+            id="foodInput"
+            width="auto"
+          />
+        </div>
+        <div>
+          <label htmlFor="gramsInput">Grams</label>
+          <Input
+            type="number"
+            required
+            margin="0"
+            placeholder="100"
+            id="gramsInput"
+            width="auto"
+          />
+        </div>
+        <Button
+          margin="0"
+          width="50px"
+          color="#DF7861"
+          label={<FaSearch />}
+          type="submit"
+        />
+      </form>
     </Container>
   );
 };
