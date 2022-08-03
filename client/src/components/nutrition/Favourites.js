c5c1efdb49c34e561641a090adf3112436afbd1b
ortori <ortori@gmail.com>
2022-08-03 12:23:55 +0300

    fixed some bugs

--- client/src/components/nutrition/Favourites.js
+++ client/src/components/nutrition/Favourites.js
@@ -28,7 +28,7 @@ const Favourites = () => {
         {loading && <Loader />}
         {favourites.length < 1 && <p>No favourites yet...</p>}
         <div className={style.container}>
-          {favourites.map((food) => {
+          {[...favourites].reverse().map((food) => {
             return <FoodCard {...food} key={food.food_id} del />;
           })}
         </div>
